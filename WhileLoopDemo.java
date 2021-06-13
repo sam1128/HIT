0d9570bde909ad55d139c556d06dc33793474b7b
sam1128 <82404886+sam1128@users.noreply.github.com>
2021-06-13 19:57:56 +0530

    Lab exercise 2
    Week 2 lab exercise

---
+++ WhileLoopDemo.java
@@ -0,0 +1,16 @@
+package hit.lab2;
+
+public class WhileLoopDemo { 
+	 public static void main(String args[]) 
+	 { 
+	 int x = 1, sum = 0; 
+
+	 while (x <= 10) { 
+
+	 sum = sum + x; 
+
+	 x++; 
+	 } 
+	 System.out.println("Summation: " + sum); 
+	 } 
+	} 
