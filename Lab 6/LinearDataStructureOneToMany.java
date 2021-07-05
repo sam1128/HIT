90673e1e423744c42c372caf89eb0760c5aff81d
sam1128 <82404886+sam1128@users.noreply.github.com>
2021-07-05 22:13:38 +0530

    Add files via upload

---
+++ Lab 6/LinearDataStructureOneToMany.java
@@ -0,0 +1,14 @@
+package hit.week6;
+import java.util.StringTokenizer;
+
+public class LinearDataStructureOneToMany {
+	public static void main(String[] args) {
+		String str="java,jee,python";
+		
+		StringTokenizer st=new StringTokenizer(str,",");
+		
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+	}
+}
